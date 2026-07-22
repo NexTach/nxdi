31249ec6e05df322f07b4016d8b8ef57693511d2
snowykte0426 <snowykte0426@naver.com>
2026-07-23 01:00:53 +0900

    refactor(database): 기존 마이그레이션 이력 정리

--- server/prisma/migrations/20260718120000_add_monthly_dividend_record_id/migration.sql
+++
@@ -1,9 +0,0 @@
-ALTER TABLE `tb_monthly_dividend_records`
-  ADD COLUMN `record_id` VARCHAR(36) NULL;
-
-UPDATE `tb_monthly_dividend_records`
-SET `record_id` = CONCAT('mdr_', REPLACE(UUID(), '-', ''))
-WHERE `record_id` IS NULL;
-
-ALTER TABLE `tb_monthly_dividend_records`
-  ADD UNIQUE INDEX `uq_monthly_dividend_records_record_id` (`record_id`);
--- server/prisma/migrations/20260718121000_require_monthly_dividend_record_id/migration.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `tb_monthly_dividend_records`
-  MODIFY COLUMN `record_id` VARCHAR(36) NOT NULL,
-  DROP COLUMN `memo`;
